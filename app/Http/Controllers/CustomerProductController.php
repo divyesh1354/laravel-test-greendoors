ac97b1e86cff388f399281c3746a65d8ed97fa73
divyesh1354 <dp192954@gmail.com>
2023-11-12 16:20:19 +0000

    dev: migration and seeder files created

---
+++ app/Http/Controllers/CustomerProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
